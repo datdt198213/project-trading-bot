bacec1eb76d061f4c779996faf103c03a283c6c9
Đặng Đạt <73807126+datdt198213@users.noreply.github.com>
2024-10-10 00:41:43 +0700

    add ref successfully

--- backend/app.ts
+++ backend/app.ts
@@ -6,7 +6,10 @@ import { Keypair } from "@mysten/sui/dist/cjs/cryptography";
 import { Ed25519Keypair, Ed25519PublicKey } from "@mysten/sui/keypairs/ed25519";
 import { bot_token } from "./config";
 import { swapToken } from "./services/contract/contract";
-import { general_menu_buttons, TelegramCallbackData } from "./services/telegram/telegram";
+import {
+    general_menu_buttons,
+    TelegramCallbackData,
+} from "./services/telegram/telegram";
 
 // Function to create a SUI wallet
 async function createSuiWallet(): Promise<Ed25519Keypair> {
@@ -17,50 +20,88 @@ async function createSuiWallet(): Promise<Ed25519Keypair> {
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(bot_token, { polling: true });
 
+bot.onText(/\/start/, async (msg: Message) => {
+    // create wallet for new members
+    const keypair = await createSuiWallet();
+    const walletAddress = keypair.toSuiAddress();
+
+    // Send welcome message with wallet address
+    await bot.sendMessage(
+        msg.chat.id,
+        `Welcome FISH BOT, your wallet is ${walletAddress}`
+    );
+
+    // Send message with menu buttons
+    await bot.sendMessage(
+        msg.chat.id,
+        "What do you want to do with the bot?",
+        general_menu_buttons
+    );
+});
+
 // New chat members
-bot.on('new_chat_members', async (msg: Message) => {
+bot.on("new_chat_members", async (msg: Message) => {
     const newMembers = msg.new_chat_members;
 
     if (newMembers) {
         for (const member of newMembers) {
-            const username = member.username || member.first_name || 'New user';
+            const username = member.username || member.first_name || "New user";
 
             // create wallet for new members
             const keypair = await createSuiWallet();
             const walletAddress = keypair.toSuiAddress();
 
             // Send welcome message with wallet address
-            await bot.sendMessage(msg.chat.id, `Welcome ${username}, your wallet is ${walletAddress}`);
+            await bot.sendMessage(
+                msg.chat.id,
+                `Welcome FISH BOT, your wallet is ${walletAddress}`
+            );
 
             // Send message with menu buttons
-            await bot.sendMessage(msg.chat.id, 'What do you want to do with the bot?', general_menu_buttons);
+            await bot.sendMessage(
+                msg.chat.id,
+                "What do you want to do with the bot?",
+                general_menu_buttons
+            );
         }
     }
 });
 
 // Button events
-bot.on('callback_query', async (callbackQuery: CallbackQuery) => {
+bot.on("callback_query", async (callbackQuery: CallbackQuery) => {
     const message = callbackQuery.message;
     const data = callbackQuery.data;
 
     if (message && data) {
         switch (data) {
             case TelegramCallbackData.BUY:
-                await bot.sendMessage(message.chat.id, 'API Token: xxxxxx');
+                await bot.sendMessage(message.chat.id, "API Token: xxxxxx");
                 break;
             case TelegramCallbackData.SELL:
-                await bot.sendMessage(message.chat.id, 'Edit Bot options...');
+                await bot.sendMessage(message.chat.id, "Edit Bot options...");
+                break;
+            case TelegramCallbackData.BUY_FISH:
+                await bot.sendMessage(message.chat.id, "Edit Bot options...");
+                break;
+            case TelegramCallbackData.VISIT_CAT_FISH:
+                await bot.sendMessage(message.chat.id, "Edit Bot options...");
                 break;
             case TelegramCallbackData.WALLET:
-                await bot.sendMessage(message.chat.id, 'Bot Settings options...');
+                await bot.sendMessage(
+                    message.chat.id,
+                    "Bot Settings options..."
+                );
                 break;
             case TelegramCallbackData.REFER_FRIENDS:
-                const userId = message.from?.id
-                const shareLink = `https://t.me/ref${userId}`
-                await bot.sendMessage(message.chat.id, `Here is your share link: ${shareLink}. Send it to your friend to earn token gift.`); // Gửi lại liên kết chia sẻ
+                const userId = message.from?.id;
+                const shareLink = `https://t.me/sui_fish_bot?start=ref_${userId}`;
+                await bot.sendMessage(
+                    message.chat.id,
+                    `Here is your share link: ${shareLink}. Send it to your friend to earn token gift.`
+                ); // Gửi lại liên kết chia sẻ
                 break;
             default:
-                await bot.sendMessage(message.chat.id, 'Invalid action.');
+                await bot.sendMessage(message.chat.id, "Invalid action.");
                 break;
         }
 
